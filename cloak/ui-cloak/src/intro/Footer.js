c8c93a28a35b0c9f3b8ce284ef0a794fa9b647c0
maaz <maazweb3@gmail.com>
2023-04-15 05:59:00 -0700

    links

--- cloak/ui-cloak/src/intro/Footer.js
+++ cloak/ui-cloak/src/intro/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
   <div class="p-4 bg-[#FFF7F7] mt-1 ">
     <ul class="flex justify-center space-x-4">
       <li class="p-2"><a href="/"><FaDiscord size={24} className='text-gray-500 ' /></a></li>
-      <li class="p-2"><a href="/"><FaGithub size={24} className='text-gray-500' /></a></li>
-      <li class="p-2"><a href="/"><FaEnvelope size={24} className='text-gray-500' /></a></li>
+      <li class="p-2"><a href="https://github.com/ScriptKiddii/Cloak"><FaGithub size={24} className='text-gray-500' /></a></li>
+      <li class="p-2"><a href="https://twitter.com/TronCloak"><FaEnvelope size={24} className='text-gray-500' /></a></li>
     </ul>
   </div>
   )
