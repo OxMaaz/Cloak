d852fee74678dba19db222f9248069cf8a09e8ab
maaz <maazweb3@gmail.com>
2023-09-27 12:26:50 -0700

    user wallet integration

--- cloak/contracts/TRC721.sol
+++ cloak/contracts/TRC721.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.6;
 
-interface IERC721 {
+interface TRC721 {
     function transferFrom(address from, address to, uint256 tokenId) external;
     function ownerOf(uint256 tokenId) external view returns (address);
 }
